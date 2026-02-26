1b5fea2ddac497c1f4f144bb4934739bff32c39a
dwivediprashant <prashantdwivedi.0219@gmail.com>
2026-02-27 00:50:12 +0530

    Added cors package

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import chatRoutes from "./routes/chat.js";
+import cors from "cors";
 //------db----
 import connectDB from "./db.js";
 connectDB();
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT;
 //middlewares
 app.use(express.json());
+app.use(cors());
 //routes
 app.use("/api", chatRoutes);
 
